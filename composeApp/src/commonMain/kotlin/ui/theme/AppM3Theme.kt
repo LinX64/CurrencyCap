4d6d1e00e69dc0eaf77df9d1b2ba0e5e6bb6231c
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-04 00:21:05 +0200

    Added background and UI refactor

---
+++ composeApp/src/commonMain/kotlin/ui/theme/AppM3Theme.kt
@@ -0,0 +1,87 @@
+package ui.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import ui.theme.colors.CurrencyColors
+
+@Composable
+fun AppM3Theme(
+    dark: Boolean,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = if (dark) appDarkColorScheme() else appLightColorScheme(),
+        content = content
+    )
+}
+
+private fun appLightColorScheme(): ColorScheme = ColorScheme(
+    primary = CurrencyColors.Blue.primary,
+    onPrimary = CurrencyColors.White,
+    primaryContainer = CurrencyColors.Blue.light,
+    onPrimaryContainer = CurrencyColors.White,
+    inversePrimary = CurrencyColors.Blue.dark,
+    secondary = CurrencyColors.Green.primary,
+    onSecondary = CurrencyColors.White,
+    secondaryContainer = CurrencyColors.Green.light,
+    onSecondaryContainer = CurrencyColors.White,
+    tertiary = CurrencyColors.Green.primary,
+    onTertiary = CurrencyColors.White,
+    tertiaryContainer = CurrencyColors.Green.light,
+    onTertiaryContainer = CurrencyColors.White,
+
+    error = CurrencyColors.Red.primary,
+    onError = CurrencyColors.White,
+    errorContainer = CurrencyColors.Red.light,
+    onErrorContainer = CurrencyColors.White,
+
+    background = CurrencyColors.White,
+    onBackground = CurrencyColors.Black,
+    surface = CurrencyColors.White,
+    onSurface = CurrencyColors.Black,
+    surfaceVariant = CurrencyColors.ExtraLightGray,
+    onSurfaceVariant = CurrencyColors.Black,
+    surfaceTint = CurrencyColors.Black,
+    inverseSurface = CurrencyColors.DarkGray,
+    inverseOnSurface = CurrencyColors.White,
+
+    outline = CurrencyColors.Gray,
+    outlineVariant = CurrencyColors.DarkGray,
+    scrim = CurrencyColors.ExtraDarkGray.copy(alpha = 0.8f)
+)
+
+private fun appDarkColorScheme(): ColorScheme = ColorScheme(
+    primary = CurrencyColors.Blue.primary,
+    onPrimary = CurrencyColors.White,
+    primaryContainer = CurrencyColors.Blue.light,
+    onPrimaryContainer = CurrencyColors.White,
+    inversePrimary = CurrencyColors.Blue.dark,
+    secondary = CurrencyColors.Green.primary,
+    onSecondary = CurrencyColors.White,
+    secondaryContainer = CurrencyColors.Green.light,
+    onSecondaryContainer = CurrencyColors.White,
+    tertiary = CurrencyColors.Green.primary,
+    onTertiary = CurrencyColors.White,
+    tertiaryContainer = CurrencyColors.Green.light,
+    onTertiaryContainer = CurrencyColors.White,
+
+    error = CurrencyColors.Red.primary,
+    onError = CurrencyColors.White,
+    errorContainer = CurrencyColors.Red.light,
+    onErrorContainer = CurrencyColors.White,
+
+    background = CurrencyColors.Black,
+    onBackground = CurrencyColors.White,
+    surface = CurrencyColors.Black,
+    onSurface = CurrencyColors.White,
+    surfaceVariant = CurrencyColors.ExtraDarkGray,
+    onSurfaceVariant = CurrencyColors.White,
+    surfaceTint = CurrencyColors.White,
+    inverseSurface = CurrencyColors.LightGray,
+    inverseOnSurface = CurrencyColors.Black,
+
+    outline = CurrencyColors.Gray,
+    outlineVariant = CurrencyColors.LightGray,
+    scrim = CurrencyColors.ExtraLightGray.copy(alpha = 0.8f)
+)
