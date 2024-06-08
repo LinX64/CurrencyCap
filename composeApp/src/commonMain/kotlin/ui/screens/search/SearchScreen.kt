21c41a7f9665c6720ea2f5258d7c123b036f52e2
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-09 00:45:49 +0200

    Searchbar

--- composeApp/src/commonMain/kotlin/ui/screens/search/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/search/SearchScreen.kt
@@ -1,15 +1,33 @@
 package ui.screens.search
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.unit.dp
+import currencycap.composeapp.generated.resources.Res
+import currencycap.composeapp.generated.resources.baseline_search_24
 import di.koinViewModel
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 internal fun SearchScreen(
@@ -18,18 +36,75 @@ internal fun SearchScreen(
 ) {
     Column(
         modifier = Modifier.fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+            .padding(padding),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//        SearchBar(
-//            value = searchViewModel.searchQuery,
-//            onValueChange = { searchViewModel.searchQuery = it },
-//            placeholder = { Text("Search") },
-//            modifier = Modifier.fillMaxWidth()
-//        )
+        SearchBarView()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SearchBarView(
+    modifier: Modifier = Modifier
+) {
+    var text by rememberSaveable { mutableStateOf("") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
+    Box(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .semantics { isTraversalGroup = true }
+    ) {
+        SearchBar(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .semantics { traversalIndex = 0f },
+            active = expanded,
+            onActiveChange = { expanded = it },
+            onQueryChange = { text = it },
+            query = text,
+            placeholder = { SearchPlaceHolder() },
+            onSearch = { expanded = false },
+            leadingIcon = { LeadingIcon() },
+        ) {
+            HandleList()
+        }
+    }
+}
+
+@Composable
+private fun HandleList() {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.semantics { traversalIndex = 1f },
+    ) {
+        val list = List(100) { "Text $it" }
+        items(count = list.size) {
+            Text(
+                text = list[it],
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            )
+        }
+    }
+}
+
+@Composable
+private fun LeadingIcon() {
+    Icon(
+        painter = painterResource(Res.drawable.baseline_search_24),
+        contentDescription = null
+    )
+}
+
+@Composable
+private fun SearchPlaceHolder() {
+    Text(
+        text = "Search for currency or coin",
+        color = MaterialTheme.colorScheme.onSurface,
+    )
+}
 
