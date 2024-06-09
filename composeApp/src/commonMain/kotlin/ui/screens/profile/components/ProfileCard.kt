9436c9950f84911a0465ccda6ab811040b376c6e
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-09 11:38:08 +0200

    Ui improvements

---
+++ composeApp/src/commonMain/kotlin/ui/screens/profile/components/ProfileCard.kt
@@ -0,0 +1,103 @@
+package ui.screens.profile.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+
+@Composable
+internal fun ProfileCard(
+    modifier: Modifier = Modifier,
+    profileImage: String = "https://www.w3schools.com/howto/img_avatar.png",
+    name: String = "John Doe",
+    email: String = "ash.qxrz@hotmail.com",
+    phone: String = "+989123456789"
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Canvas(
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.TopEnd)
+                .offset(x = 15.dp, y = (-20).dp)
+        ) {
+            drawCircle(
+                color = Color.LightGray.copy(alpha = 0.2f),
+                radius = size.minDimension / 2
+            )
+        }
+
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+                    shape = RoundedCornerShape(20.dp)
+                )
+        ) {
+            Column(
+                modifier = Modifier.padding(25.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(55))
+                        .padding(top = 16.dp),
+                    model = profileImage,
+                    contentDescription = null
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = email,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+
+                    Text(
+                        text = phone,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+        }
+    }
+}
