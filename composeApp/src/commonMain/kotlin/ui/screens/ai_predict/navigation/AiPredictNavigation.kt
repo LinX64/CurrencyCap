c878acf84db32e63a9d8f50d41487248c77198fc
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-27 14:04:45 +0200

    Added ai nav

---
+++ composeApp/src/commonMain/kotlin/ui/screens/ai_predict/navigation/AiPredictNavigation.kt
@@ -0,0 +1,17 @@
+package ui.screens.ai_predict.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import dev.chrisbanes.haze.HazeState
+import ui.navigation.NavRoutes
+import ui.screens.ai_predict.AiPredictScreen
+
+fun NavController.navigateToAiPredictScreen() = navigate(NavRoutes.AI_PREDICTION)
+
+fun NavGraphBuilder.aiPredictScreen(padding: PaddingValues, hazeState: HazeState) {
+    composable(NavRoutes.AI_PREDICTION) {
+        AiPredictScreen(padding = padding, hazeState = hazeState)
+    }
+}
