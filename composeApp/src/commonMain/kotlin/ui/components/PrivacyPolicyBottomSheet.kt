9674635482a95a8037d6ea1399b67b178a9d2f85
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-07 17:32:02 +0200

    Hiding

--- composeApp/src/commonMain/kotlin/ui/components/PrivacyPolicyBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/ui/components/PrivacyPolicyBottomSheet.kt
@@ -1,6 +1,9 @@
 package ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -8,9 +11,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,6 +35,11 @@ internal fun PrivacyPolicyBottomSheet(
     content: @Composable () -> Unit
 ) {
     val scope = rememberCoroutineScope()
+
+    val coroutineScope = rememberCoroutineScope()
+    val hideModalBottomSheet: () -> Unit =
+        { coroutineScope.launch { scaffoldState.bottomSheetState.hide() } }
+
     BottomSheetScaffold(
         modifier = modifier.fillMaxWidth(),
         scaffoldState = scaffoldState,
@@ -35,20 +47,46 @@ internal fun PrivacyPolicyBottomSheet(
         contentColor = MaterialTheme.colorScheme.onSurface,
         sheetShape = RoundedCornerShape(topStart = 55.dp, topEnd = 55.dp),
         sheetContent = {
-            Column(
-                Modifier
-                    .navigationBarsPadding()
-                    .padding(bottom = 10.dp)
-                    .heightIn(max = 700.dp)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                content()
+            Box {
+                BottomSheetHeader(
+                    title = "Privacy Policy",
+                    onCloseClick = { hideModalBottomSheet() }
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .navigationBarsPadding()
+                        .heightIn(max = 700.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    content()
+                }
             }
         }) { }
 
     LaunchedEffect(scaffoldState.bottomSheetState) {
         scope.launch {
             scaffoldState.bottomSheetState.expand()
+        }
+    }
+}
+
+@Composable
+private fun BottomSheetHeader(
+    title: String,
+    onCloseClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        horizontalArrangement = Arrangement.End
+    ) {
+        IconButton(onClick = onCloseClick) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Close",
+                tint = MaterialTheme.colorScheme.onSurface
+            )
         }
     }
 }
