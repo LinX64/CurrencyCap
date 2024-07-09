a8b288c20fe41996bb9a4b90377019198d0f55f8
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-09 20:07:01 +0200

    Market tab

--- composeApp/src/androidMain/kotlin/com/client/currencycap/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/client/currencycap/Previews.kt
@@ -1,14 +1,12 @@
 package com.client.currencycap
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import dev.chrisbanes.haze.HazeState
-import ui.screens.main.overview.OverviewScreen
 import ui.screens.main.overview.OverviewState
+import ui.screens.main.overview.components.tabs.MarketContent
 import util.getDummyBonbastRates
 import util.getDummyCryptoItems
 import util.getDummyMarketRates
@@ -22,13 +20,8 @@ private fun ExchangePreview() {
 
     KoinPreview {
         Column {
-            OverviewScreen(
-                padding = PaddingValues(0.dp),
-                hazeState = hazeState,
-                onSearchCardClicked = { /*TODO*/ },
+            MarketContent(
                 onNewsItemClick = { /*TODO*/ },
-                onCircleButtonClicked = { /*TODO*/ },
-                onRetry = { /*TODO*/ },
                 state = OverviewState.Success(
                     bonbastRates = getDummyBonbastRates(),
                     cryptoRates = getDummyCryptoItems(),
--- composeApp/src/commonMain/kotlin/ui/screens/main/overview/components/tabs/MarketTab.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/main/overview/components/tabs/MarketTab.kt
@@ -1,8 +1,6 @@
 package ui.screens.main.overview.components.tabs
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
@@ -22,13 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import domain.model.main.Crypto
 import ui.components.CenteredColumn
-import ui.components.GlassCard
 import ui.screens.main.overview.OverviewState
 import ui.screens.main.overview.OverviewState.Success
 import util.formatNumber
@@ -51,16 +49,16 @@ internal fun MarketContent(
         Spacer(modifier = Modifier.height(8.dp))
 
         LazyVerticalGrid(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxSize().height(220.dp),
             columns = GridCells.Fixed(2),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             when (state) {
                 is Success -> {
-                    val cryptoItems = state.cryptoRates
-                    items(cryptoItems.size) { item ->
-                        val cryptoItem = cryptoItems[item]
+                    val cryptoRates = state.cryptoRates.take(4)
+                    items(cryptoRates.size) { item ->
+                        val cryptoItem = cryptoRates[item]
                         CryptoGridItem(cryptoItem)
                     }
                 }
@@ -81,53 +79,57 @@ internal fun MarketContent(
 private fun CryptoGridItem(
     cryptoItem: Crypto
 ) {
-    val randomColor = cryptoItem.name.hashCode()
-    GlassCard {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+    Column(
+        modifier = Modifier
+            .wrapContentWidth()
+            .padding(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(24.dp)
-                        .clip(CircleShape)
-                        .background(Color(randomColor))
+            AsyncImage(
+                modifier = Modifier.size(32.dp).clip(CircleShape),
+                model = cryptoItem.image,
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column {
+                Text(
+                    text = cryptoItem.name,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                    fontWeight = FontWeight.Medium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
 
-                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = cryptoItem.symbol.uppercase(),
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize
+                )
 
-                Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
                     Text(
-                        text = cryptoItem.name,
+                        text = "$${formatNumber(cryptoItem.currentPrice)}",
                         color = MaterialTheme.colorScheme.onSurface,
                         fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-                        fontWeight = FontWeight.Medium,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
+                        fontWeight = FontWeight.Medium
                     )
+
+
                     Text(
-                        text = cryptoItem.symbol.uppercase(),
-                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                        text = "MC: $${formatNumber(cryptoItem.marketCap)}",
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
                         fontSize = MaterialTheme.typography.bodySmall.fontSize
                     )
                 }
             }
-
-            Text(
-                text = "$${formatNumber(cryptoItem.currentPrice)}",
-                color = MaterialTheme.colorScheme.onSurface,
-                fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-                fontWeight = FontWeight.Medium
-            )
-            Text(
-                text = "MC: $${formatNumber(cryptoItem.marketCap)}",
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
-                fontSize = MaterialTheme.typography.bodySmall.fontSize
-            )
         }
     }
 }
