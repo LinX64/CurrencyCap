526693dba293c3494ba8a3675e136f01bbd47eee
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-18 23:36:50 +0200

    Fixing not init app

--- composeApp/src/androidMain/kotlin/com/client/currencycap/MyApp.kt
+++ composeApp/src/androidMain/kotlin/com/client/currencycap/MyApp.kt
@@ -2,6 +2,7 @@ package com.client.currencycap
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import ui.common.ContextProvider
 import ui.common.KoinInitializer
 import util.ApplicationComponent
 
@@ -9,6 +10,7 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        ContextProvider().create(this)
         KoinInitializer.initializeKoin()
 
         FirebaseApp.initializeApp(this)
