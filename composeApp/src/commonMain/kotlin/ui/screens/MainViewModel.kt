df373d393b17af00ccb17ffc19894a90d85c8187
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-21 17:26:21 +0200

    Code refactor main

--- composeApp/src/commonMain/kotlin/ui/screens/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/MainViewModel.kt
@@ -5,8 +5,15 @@ import androidx.lifecycle.viewModelScope
 import domain.repository.UserPreferences
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import ui.screens.MainState.Loading
+import ui.screens.MainState.LoggedIn
+import ui.screens.MainState.NotLoggedIn
+import ui.theme.ThemeMode
 
 class MainViewModel(
     private val userPreferences: UserPreferences
@@ -14,21 +21,20 @@ class MainViewModel(
 
     private val _state = MutableStateFlow<MainState>(MainState.Idle)
     val state = _state.asStateFlow()
-
-//    val isDarkMode = userPreferences.isDarkMode()
-//        .map { isDarkMode -> if (isDarkMode) ThemeMode.DARK else ThemeMode.LIGHT }
-//        .stateIn(
-//            scope = viewModelScope,
-//            started = SharingStarted.Lazily,
-//            initialValue = ThemeMode.SYSTEM
-//        )
+    val isDarkMode = userPreferences.isDarkMode()
+        .map { isDarkMode -> if (isDarkMode) ThemeMode.DARK else ThemeMode.LIGHT }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = ThemeMode.SYSTEM
+        )
 
     init {
         checkUserLoginStatus()
     }
 
     private fun checkUserLoginStatus() {
-        _state.value = MainState.Loading
+        _state.value = Loading
 
         viewModelScope.launch {
             delay(1500)
@@ -36,20 +42,20 @@ class MainViewModel(
             val userLoggedIn = userPreferences.isUserLoggedIn()
             if (userLoggedIn) {
                 val uid = userPreferences.getUserUid()
-                _state.value = MainState.LoggedIn(uid)
-            } else _state.value = MainState.NotLoggedIn
+                _state.value = LoggedIn(uid)
+            } else _state.value = NotLoggedIn
         }
     }
 
     fun onLoginSuccess() {
         viewModelScope.launch {
             val uid = userPreferences.getUserUid()
-            _state.value = MainState.LoggedIn(uid)
+            _state.value = LoggedIn(uid)
         }
     }
 
     fun navigateToLanding() {
-        _state.value = MainState.NotLoggedIn
+        _state.value = NotLoggedIn
     }
 }
 
