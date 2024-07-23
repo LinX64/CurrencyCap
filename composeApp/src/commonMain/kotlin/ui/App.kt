035e27bcf19f8fc14d39a6057b5eca560612af6c
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-23 18:22:23 +0200

    Code cleanup

--- composeApp/src/commonMain/kotlin/ui/App.kt
+++ composeApp/src/commonMain/kotlin/ui/App.kt
@@ -1,8 +1,9 @@
 package ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import di.koinViewModel
 import ui.components.main.LoggedInSection
 import ui.components.main.NotLoggedInSection
@@ -16,11 +17,17 @@ import ui.screens.initial.splash.SplashScreen
 internal fun App(
     mainViewModel: MainViewModel = koinViewModel<MainViewModel>(),
 ) {
-    val mainState by mainViewModel.state.collectAsState()
+    val mainState by mainViewModel.appState.collectAsStateWithLifecycle()
     when (mainState) {
         Loading -> SplashScreen()
         is LoggedIn -> LoggedInSection(mainViewModel = mainViewModel)
         is NotLoggedIn -> NotLoggedInSection(mainViewModel = mainViewModel)
         else -> Unit
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            mainViewModel.clearState()
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/ui/screens/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/MainViewModel.kt
@@ -6,27 +6,23 @@ import domain.repository.UserPreferences
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import ui.screens.MainState.Loading
 import ui.screens.MainState.LoggedIn
 import ui.screens.MainState.NotLoggedIn
-import ui.theme.ThemeMode
 
 class MainViewModel(
     private val userPreferences: UserPreferences
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<MainState>(MainState.Idle)
-    val state = _state.asStateFlow()
-    val isDarkMode = userPreferences.isDarkMode()
-        .map { isDarkMode -> if (isDarkMode) ThemeMode.DARK else ThemeMode.LIGHT }
+    val appState: StateFlow<MainState> = _state
         .stateIn(
             scope = viewModelScope,
-            started = SharingStarted.Lazily,
-            initialValue = ThemeMode.SYSTEM
+            started = SharingStarted.WhileSubscribed(1_000L),
+            initialValue = MainState.Idle
         )
 
     init {
@@ -40,8 +36,7 @@ class MainViewModel(
             delay(1500)
 
             val userLoggedIn = userPreferences.isUserLoggedIn()
-            if (userLoggedIn) _state.value = LoggedIn
-            else _state.value = NotLoggedIn
+            if (userLoggedIn) _state.value = LoggedIn else _state.value = NotLoggedIn
         }
     }
 
@@ -51,6 +46,10 @@ class MainViewModel(
 
     fun navigateToLanding() {
         _state.value = NotLoggedIn
+    }
+
+    fun clearState() {
+        _state.value = MainState.Idle
     }
 }
 
