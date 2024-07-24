d5d4f4b7683edf3786ebc314c24505a4b71667fa
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-25 00:44:57 +0200

    Loading issue fix

--- composeApp/src/commonMain/kotlin/ui/screens/main/overview/OverviewViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/main/overview/OverviewViewModel.kt
@@ -22,7 +22,7 @@ class OverviewViewModel(
 ) : MviViewModel<OverviewViewEvent, OverviewState, OverviewNavigationEffect>(OverviewState.Loading) {
 
     init {
-        loadCombinedRates()
+        handleEvent(OnLoadRates)
     }
 
     override fun handleEvent(event: OverviewViewEvent) {
@@ -33,6 +33,8 @@ class OverviewViewModel(
     }
 
     private fun loadCombinedRates() {
+        setState { OverviewState.Loading }
+
         val ratesFlow = mainRepository.getAllRates()
         val newsFlow = newsRepository.getNews().map {
             if (it is NetworkResult.Success) it.data else emptyList()
