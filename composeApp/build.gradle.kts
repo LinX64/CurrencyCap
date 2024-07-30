beb897d615764f638b1a156860a41e072b296bb3
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-07-30 22:23:40 +0200

    Bump v1.1.4 - code9

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -127,7 +127,7 @@ android {
         applicationId = "com.client.currencycap"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = "1.1.4"
     }
 
