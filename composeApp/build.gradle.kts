bd70531c2d6f2a34074ee9f307ebfb7f67c82333
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-08-05 20:01:12 +0200

    Removed persistent cache

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,7 +98,6 @@ kotlin {
             implementation(libs.bundles.datastore)
             implementation(libs.bundles.ktor)
             implementation(libs.kotlinx.serialization)
-            implementation(libs.kachetor)
             api(libs.koin.core)
             implementation(libs.koin.compose)
         }
