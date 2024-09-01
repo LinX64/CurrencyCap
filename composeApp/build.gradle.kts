f162e303cab13af3637cb1dfd969b973d3a2933b
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-09-01 20:24:05 +0200

    Bump 1.1.6

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -128,8 +128,8 @@ android {
         applicationId = "com.client.currencycap"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.1.5"
+        versionCode = 16
+        versionName = "1.1.6"
     }
 
     packaging {
