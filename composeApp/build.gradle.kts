baba23665f135796409c8fce75d528a0f304d75c
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-09-01 20:18:06 +0200

    Notification

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 kotlin {
     androidTarget {
         @OptIn(ExperimentalKotlinGradlePluginApi::class) compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
